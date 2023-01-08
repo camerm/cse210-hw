d08a3151b1890e71302d5ba18141646357a3bd19
camerm <cameron.ocm@gmail.com>
2023-01-07 21:28:21 -0500

    finished pre 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is yor grade?:");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string score = "";
+
+        if (number >= 90)
+        {
+            score = "A";
+        }
+        else if (number >= 80)
+        {
+            score = "B";
+        }
+        else if (number >= 70)
+        {
+            score = "C";
+        }
+        else if (number >= 60)
+        {
+            score = "D";
+        }
+        else
+        {
+            score = "F";
+        }
+
+        Console.WriteLine($"Your grade is {score}");
+
+
+        // Messages for pass or not pass
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations You passed!");
+        }
+        else
+        {
+            Console.WriteLine("sorry. Better luck next time!");
+        }
     }
 }
