e1b9fe7626c1e0b7c148f7b9731e15c93ac66a3c
camerm <cameron.ocm@gmail.com>
2023-01-08 17:27:54 -0500

    finished 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of number, type 0 when finished.");
+
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter number:");
+            string x = Console.ReadLine();
+            userNumber = int.Parse(x);
+
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
