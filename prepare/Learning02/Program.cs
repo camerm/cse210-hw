4e2265153fe14e8d28e3e9e19406c5f3d999fe62
camerm <cameron.ocm@gmail.com>
2023-01-14 00:53:15 -0500

    finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,5 +12,20 @@ class Program
         job1._startYear = 2015;
         job1._endYear = 2019;
         job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitlle = "civil engineer";
+        job2._company = "WSP";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Oliverio Cameron";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+
+        myResume.Display();
     }
 }
