109ccd4eb75c63cc0c151cb79d9d832dcc1a0e40
camerm <cameron.ocm@gmail.com>
2023-01-14 22:06:28 -0500

    update

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,37 @@
+public class PromptGenerator
+{
+    
+    static Void Main()
+    {
+        int userNumber = -1;
+        while (userNumber != 5)
+        {
+            Console.WriteLine("Please Select one of the following choices:");
+            string x = Console.ReadLine();
+            userNumber = int.Parse(x);
+
+            //Add in the list only the numbers diferent of 0
+            if (userNumber !=1)
+            {
+
+            }
+            else if (userNumber !=2)
+            {
+
+            }
+            else if (userNumber !=3)
+            {
+
+            }
+            else if (userNumber !=4)
+            {
+
+            }
+            else if (userNumber !=6)
+            {
+
+            }
+        }
+    }
+
+}
