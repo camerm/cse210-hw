6b75448c3f25c4bd74d03459c111758daf9065e8
camerm <cameron.ocm@gmail.com>
2023-01-14 22:07:33 -0500

    update

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -6,6 +6,7 @@ public class PromptGenerator
         int userNumber = -1;
         while (userNumber != 5)
         {
+            
             Console.WriteLine("Please Select one of the following choices:");
             string x = Console.ReadLine();
             userNumber = int.Parse(x);
