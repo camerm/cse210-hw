95048bda343d3a1759d308830911e7b19fc69b01
camerm <cameron.ocm@gmail.com>
2023-01-30 00:36:18 -0500

    update

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -3,5 +3,13 @@ public class BreathingActivity
     private string _breatheIn;
 
     private string _breatheout;
+
+    
+    public BreathingActivity() : base()
+    {
+        SetnameActivity("Breathing Activity");
+        SetdescriptionActivity("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+
+    }
     
 }
--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,4 +1,11 @@
 public class ListeningActivity
 {
-    private string
+    private string dg;
+
+    public ListingActivity() : base()
+    {
+        SetnameActivity("Listing Activity");
+        SetdescriptionActivity("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+
+    }
 }
