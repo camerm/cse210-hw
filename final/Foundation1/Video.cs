a4035a2a2550a7cf57ea86671df4d05ef68f4db8
camerm <cameron.ocm@gmail.com>
2023-02-15 09:49:37 -0500

    update

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Video
+{
+
+    private string _tittle;
+    private string _autor;
+    private int _duration;
+
+    public Video (string tittle, string autor, int duration)
+    {
+        _tittle = tittle;
+        _autor = autor;
+        _duration = duration;
+    }
+
+
+}
