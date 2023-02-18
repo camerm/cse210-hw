39e40d89228ef97c170e70ffa23a73897e13cc6d
camerm <cameron.ocm@gmail.com>
2023-02-18 17:33:05 -0500

    update

---
+++ final/Foundation3/Events.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Event
+{
+    private string _tittle;
+    private string _description;
+    private string _date;
+    private string _time;
+    private string _address;
+    private string _type;
+
+    public Event(string tittle, string description, string date, string time, string address)
+    {
+        _tittle = tittle;
+        _description = description;
+        _date = date;
+        _time = time;
+        _type = type;
+        _address = address;
+    
+    }
+
+    public string StandartDetails()
+    {
+
+    }
+    public string FullDetails()
+    {
+
+    }
+    public string ShortDetails()
+    {
+
+    }
+
+}
